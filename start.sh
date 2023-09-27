8e63e44ef2773d73cac716df336d35295283594e
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2023-09-27 20:53:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/RoyalKrrishna/MdiskWalaBot.git /MdiskWalaBot
-cd /MdiskWalaBot
+git clone https://github.com/ROYAL-JATT/MdiskSearchBot.git /MdiskSearchBot
+cd /MdiskSearchBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
